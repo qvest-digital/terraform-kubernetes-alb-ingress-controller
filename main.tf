59407e1cb4e555d09dd01580f95b0c38ff0ec52c
Bret Mogilefsky <bret.mogilefsky@gsa.gov>
2021-01-13 22:23:02 -0800

    Enable setting target ARNs to "adopt" existing ALBs
    See docs at https://kubernetes-sigs.github.io/aws-load-balancer-controller/latest/guide/targetgroupbinding/targetgroupbinding/

--- main.tf
+++ main.tf
@@ -24,6 +24,12 @@ data "aws_eks_cluster" "selected" {
   name  = var.k8s_cluster_name
 }
 
+# Authentication data for that cluster
+data "aws_eks_cluster_auth" "selected" {
+  count = var.k8s_cluster_type == "eks" ? 1 : 0
+  name  = var.k8s_cluster_name
+}
+
 data "aws_iam_policy_document" "ec2_assume_role" {
   count = var.k8s_cluster_type == "vanilla" ? 1 : 0
   statement {
@@ -434,3 +440,80 @@ resource "helm_release" "not_using_iamserviceaccount" {
   depends_on = [var.alb_controller_depends_on]
 }
 
+# Generate a kubeconfig file for the EKS cluster to use in provisioners
+data "template_file" "kubeconfig" {
+  template = <<-EOF
+    apiVersion: v1
+    kind: Config
+    current-context: terraform
+    clusters:
+    - name: ${data.aws_eks_cluster.selected[0].name}
+      cluster:
+        certificate-authority-data: ${data.aws_eks_cluster.selected[0].certificate_authority.0.data}
+        server: ${data.aws_eks_cluster.selected[0].endpoint}
+    contexts:
+    - name: terraform
+      context:
+        cluster: ${data.aws_eks_cluster.selected[0].name}
+        user: terraform
+    users:
+    - name: terraform
+      user:
+        token: ${data.aws_eks_cluster_auth.selected[0].token}
+  EOF
+}
+
+# Since the kubernetes_provider cannot yet handle CRDs, we need to set any
+# supplied TargetGroupBinding using a null_resource.
+#
+# The method use below for securely specifying the kubeconfig to provisioners
+# without spilling secrets into the logs comes from:
+# https://medium.com/citihub/a-more-secure-way-to-call-kubectl-from-terraform-1052adf37af8
+
+resource "null_resource" "supply_target_group_arns_no_iam" {
+  count = (var.alb_target_group_arns != "" && var.k8s_cluster_type == "vanilla") ? 1 : 0
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      KUBECONFIG = base64encode(data.template_file.kubeconfig.rendered)
+    }
+    command     = <<EOF
+cat <<YAML | kubectl -n ${var.k8s_namespace} --kubeconfig <(echo $KUBECONFIG | base64 --decode) apply -f -
+apiVersion: elbv2.k8s.aws/v1beta1
+kind: TargetGroupBinding
+metadata:
+  name:
+spec:
+  serviceRef:
+    name: awesome-service # route traffic to the awesome-service
+    port: 80
+  targetGroupARN: ${var.alb_target_group_arns}
+YAML
+EOF
+  }
+  depends_on = [ helm_release.not_using_iamserviceaccount ]
+}
+
+resource "null_resource" "supply_target_group_arns_iam" {
+  count = (var.alb_target_group_arns != "" && var.k8s_cluster_type == "eks") ? 1 : 0
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      KUBECONFIG = base64encode(data.template_file.kubeconfig.rendered)
+    }
+    command     = <<EOF
+cat <<YAML | kubectl -n ${var.k8s_namespace} --kubeconfig <(echo $KUBECONFIG | base64 --decode) apply -f -
+apiVersion: elbv2.k8s.aws/v1beta1
+kind: TargetGroupBinding
+metadata:
+  name:
+spec:
+  serviceRef:
+    name: awesome-service # route traffic to the awesome-service
+    port: 80
+  targetGroupARN: ${var.alb_target_group_arns}
+YAML
+EOF
+  }
+  depends_on = [ helm_release.using_iamserviceaccount ]
+}
