d05f68a2216b5998b2543820e171b9292ce3a084
hbenzekri <13715994+hbenzekri@users.noreply.github.com>
2021-10-01 17:46:34 +0100

    remove commented/irrelevant code

--- main.tf
+++ main.tf
@@ -401,9 +401,6 @@ resource "kubernetes_cluster_role_binding" "this" {
   }
 }
 
-locals {
-  
-}
 resource "helm_release" "alb_controller" {
 
   name       = "aws-load-balancer-controller"
@@ -413,22 +410,6 @@ resource "helm_release" "alb_controller" {
   namespace  = var.k8s_namespace
   atomic     = true
   timeout    = 900
- # dynamic "set" {
- #   for_each = {
- #     "clusterName"           = var.k8s_cluster_name
- #     "serviceAccount.create" = (var.k8s_cluster_type != "eks")
- #     "serviceAccount.name"   = (var.k8s_cluster_type == "eks") ? kubernetes_service_account.this.metadata[0].name : null
- #     "region"                = local.aws_region_name
- #     "vpcId"                 = local.aws_vpc_id
- #     "hostNetwork"           = var.enable_host_networking
- #   }
- #   content {
- #     name  = set.key
- #     value = set.value
- #   }
- # }
-  
-  
   values = [
     yamlencode({
       "clusterName" : var.k8s_cluster_name,
